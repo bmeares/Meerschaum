2699537881a1679a128082af0c836e0b8bf14e75
bmeares <bennett.meares@gmail.com>
2020-07-09 23:59:41 -0400

    First commit

---
+++ meerschaum/Pipe/__init__.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+"""
+Pipes are the primary data access objects in the Meerschaum system
+"""
+
+class Pipe:
+    def __init__(self):
+        pass
+
---
+++ meerschaum/__init__.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+
+from ._version import __version__
+from .config import header as __doc__
