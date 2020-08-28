46e1e6ba6063a82b58dab73c07631dd69bf0bec1
bmeares <bennett.meares@gmail.com>
2020-08-28 00:55:35 -0400

    bugfixes, should work now

--- src/meerschaum/__main__.py
+++ src/meerschaum/__main__.py
@@ -1,8 +1,21 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 # vim:fenc=utf-8
+"""
+mrsm CLI entrypoint
+"""
 
-from meerschaum.main import main
-import sys
+def main(sysargs=None):
+    from meerschaum.connectors import SQLConnector
+    connector = SQLConnector()
+    import pandas as pd
+    #  df = pd.DataFrame({})
+    
+    df = connector.read("SELECT * FROM m_home_temp_hist_raw")
+    result = connector.to_sql(df, name="new_table")
+    print(result)
+
+
 if __name__ == "__main__":
+    import sys
     main(sys.argv[1:])
