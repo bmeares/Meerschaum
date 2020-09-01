82a74418d2fe71c8f734ee968c3fc93c23573472
bmeares <bennett.meares@gmail.com>
2020-09-01 15:13:01 -0400

    tweak

--- src/meerschaum/__init__.py
+++ src/meerschaum/__init__.py
@@ -4,6 +4,8 @@
 
 from meerschaum.config import __version__
 from meerschaum.config import __doc__
+### import connectors package
+import meerschaum.connectors
 
 ### ignore module warnings
 import warnings
