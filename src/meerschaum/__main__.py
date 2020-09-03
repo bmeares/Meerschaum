2cc94f5c85e74e1cd1f8524a2166f416fb77bf66
bmeares <bennett.meares@gmail.com>
2020-09-03 12:33:41 -0400

    fixed arg parsing

--- src/meerschaum/__main__.py
+++ src/meerschaum/__main__.py
@@ -7,11 +7,14 @@
 mrsm CLI entrypoint
 """
 
-def main(sysargs=[]):
+def main():
+    import sys
     from meerschaum.actions import entry, shell
+    sysargs = sys.argv[1:]
     if len(sysargs) == 0: return shell.cmdloop()
-    return entry(sysargs)
+    return_tuple = entry(sysargs)
+    if isinstance(return_tuple, tuple) and not return_tuple[0]:
+        print(f"Error message: {return_tuple[1]}")
 
 if __name__ == "__main__":
-    import sys
-    main(sys.argv[1:])
+    main()
