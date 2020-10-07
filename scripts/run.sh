d9d405eec80ffe0572003fcce66e29160c9b2162
bmeares <bennett.meares@gmail.com>
2020-10-07 04:26:01 -0400

    Dockerfile edits

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,4 +4,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 . "$DIR"/config.sh
 
 ### run temporary container and mount src/ to /src/
-docker run -it --rm -v "$src":/src -p "$port:$port" "$image" "$@"
+# docker run -it --rm -v "$src":/src -p "$port:$port" "$image" "$@"
+docker run -it --rm -v "$src":/src "$image" "$@"
