1c421a4ed31e1469eccb446bca62160adf381b7c
bmeares <bennett.meares@gmail.com>
2021-02-08 14:00:50 -0500

    Removed docs from tracking

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -3,8 +3,11 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 . "$DIR"/config.sh
 
-### Build technical documentation
 cd "$PARENT"
+### Install docs dependencies
+python -m meerschaum upgrade packages docs -y
+
+### Build technical documentation
 python -m pdoc --html --output-dir docs/pdoc meerschaum --force
 
 ### Build general documentation
