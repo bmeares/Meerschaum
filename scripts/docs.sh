3dba9a1abb116ff025159d5a3a8de654a749871d
bmeares <bennett.meares@gmail.com>
2021-02-10 06:34:33 +0000

    TODO add flag to bypass virtual environments in upgrade packages command

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -4,12 +4,15 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 . "$DIR"/config.sh
 
 cd "$PARENT"
-### Install docs dependencies
-python -m meerschaum upgrade packages docs -y
+### Install docs dependencies.
+# python -m meerschaum upgrade packages docs -y
 
-### Build technical documentation
+### Jump into the Meerschaum virtual environment.
+# source ~/.config/meerschaum/venvs/mrsm/bin/activate
+
+### Build technical documentation.
 python -m pdoc --html --output-dir docs/pdoc meerschaum --force
 
-### Build general documentation
+### Build general documentation.
 cd "$PARENT"/docs
 python -m mkdocs build
