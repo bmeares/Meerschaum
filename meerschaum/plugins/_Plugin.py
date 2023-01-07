988a6cb79468e405c6dea48a6f080a979a576136
TrellixVulnTeam <charles.mcfarland@trellix.com>
2023-01-07 02:31:40 +0000

    Adding tarfile member sanitization to extractall()

--- meerschaum/plugins/_Plugin.py
+++ meerschaum/plugins/_Plugin.py
@@ -296,7 +296,26 @@ class Plugin:
                 dprint(f"Found existing version '{old_version}' for plugin '{self}'.")
         try:
             with tarfile.open(self.archive_path, 'r:gz') as tarf:
-                tarf.extractall(temp_dir)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner) 
+                    
+                
+                safe_extract(tarf, temp_dir)
         except Exception as e:
             warn(e)
             return False, f"Failed to extract plugin '{self.name}'."
