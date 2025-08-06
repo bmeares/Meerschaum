09a4a45dde2eff077369cdb3a099c34e9add73ee
bmeares <bennett.meares@gmail.com>
2025-08-06 11:59:12 -0400

    Add back the CLI daemon logs refresh thread to handle the startup race conditio

--- meerschaum/_internal/cli/entry.py
+++ meerschaum/_internal/cli/entry.py
@@ -121,6 +121,8 @@ def entry_with_daemon(
 
         time.sleep(refresh_seconds)
 
+    start_cli_logs_refresh_thread(daemon_ix, refresh_seconds=1)
+
     try:
         log = worker.job.daemon.rotating_log
         worker.job.monitor_logs(
@@ -182,3 +184,30 @@ def entry_with_daemon(
     success = (worker_data or {}).get('success', False)
     message = (worker_data or {}).get('message', "Failed to retrieve message from CLI worker.")
     return success, message
+
+
+def touch_cli_logs_loop(ix: int, refresh_seconds: Union[int, float, None] = None):
+    """
+    Touch the CLI daemon's logs to refresh the logs monitoring.
+    """
+    from meerschaum._internal.cli.workers import ActionWorker
+    worker = ActionWorker(ix, refresh_seconds=refresh_seconds)
+
+    while True:
+        worker.job.daemon.rotating_log.touch()
+        time.sleep(worker.refresh_seconds)
+
+
+def start_cli_logs_refresh_thread(ix: int, refresh_seconds: Union[int, float, None] = None):
+    """
+    Spin up a daemon thread to refresh the CLI's logs.
+    """
+    from meerschaum.utils.threading import Thread
+    refresh_seconds = refresh_seconds or mrsm.get_config('system', 'cli', 'refresh_seconds')
+    thread = Thread(
+        target=touch_cli_logs_loop,
+        args=(ix,),
+        kwargs={'refresh_seconds': refresh_seconds},
+        daemon=True,
+    )
+    thread.start()
